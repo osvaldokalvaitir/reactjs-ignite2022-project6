229c55f85c150ba7e685b51232fdc300a6ac259b
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-02-02 21:45:52 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/reactjs-ignite2022-project5/'
+    }
+
+    return config
   }
 }
